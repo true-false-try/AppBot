ab2019d8666d1226143df7b585b7a4601cf7cb52
true-false-try <6mateo6ch@gmail.com>
2024-05-22 12:17:57 +0300

    [VAULT-2] - created method with getKey and realized custom exception

--- src/main/java/com/bot/psybot/vault/service/VaultService.java
+++ src/main/java/com/bot/psybot/vault/service/VaultService.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @RequiredArgsConstructor
 public class VaultService {
-
     public VaultResponse fetchMongoConfigFromVault() {
         String url = "http://localhost:8095/vault/getMongoConfig?token-mongo=s.6x50Cm9Si8CXGGtz7dtabNO6&path-mongo=/mongodb/mental_bot";
         RestTemplate restTemplate = new RestTemplate();
